c77473b27a8e7ba4fa5585cc2bcda122f5e4a4dc
Vlad Melnyk <vlad22523@gmail.com>
2024-10-31 15:20:35 +0200

    template

---
+++ src/components/ScreenPage/ScreenPage.jsx
@@ -0,0 +1,7 @@
+import s from "./ScreenPage.module.css";
+
+const ScreenPage = () => {
+  return <main className={s.main}></main>;
+};
+
+export default ScreenPage;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,18 @@
+import Header from "../../components/Header/Header.jsx";
+import ScreenPage from "../../components/ScreenPage/ScreenPage.jsx";
+import Sidebar from "../../components/Sidebar/Sidebar.jsx";
+import s from "./HomePage.module.css";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className={s.wrapper}>
+      <Sidebar />
+      <div className={s.content}>
+        <Header />
+        <ScreenPage />
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
