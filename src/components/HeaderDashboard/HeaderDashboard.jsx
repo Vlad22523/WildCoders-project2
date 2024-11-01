b60fd9ef1f7830b4598d2098eac987b6329eafe2
Serhii Syrov <surov.sergey.m@gmail.com>
2024-11-01 15:52:08 +0100

    column

--- src/components/HeaderDashboard/HeaderDashboard.jsx
+++ src/components/HeaderDashboard/HeaderDashboard.jsx
@@ -2,7 +2,7 @@ import Filters from "../Filters/Filters.jsx"
 import s from "./HeaderDashboard.module.css"
 
 const HeaderDashboard = () => {
-  const isBoardCreated = false;
+  const isBoardCreated = true;
 
   return (
     <div className={`${s.wrapper} ${isBoardCreated ? s.visible : ''}`}>{isBoardCreated && <p className={s.title}>Name of board</p>}
--- src/components/ScreensPage/ScreensPage.jsx
+++ src/components/ScreensPage/ScreensPage.jsx
@@ -8,14 +8,46 @@ const ScreensPage = () => {
 
   return (
     <main className={s.main}>
-      <HeaderDashboard />
+      <div className={s.header}>
+        <HeaderDashboard />
+      </div>
       <div
         className={`${s.mainWrapper} ${
           isBoardCreated ? s.boardCreated : s.noBoard
         }`}
       >
         {isBoardCreated ? (
-          <div className={s.columnWrapper}>
+          <div className={s.columnContainer}>
+            <div className={s.columnWrapper}>
+              <button className={`${s.button} ${s.buttonColumn}`} type="button">
+                <span className={s.buttonSpan}>To Do</span>
+                <div className={s.svgWrapperColumn}>
+                  <SvgIcon
+                    name="icon-pencil"
+                    width="16"
+                    height="16"
+                    className={s.iconColumn}
+                  />
+                  <SvgIcon
+                    name="icon-trash"
+                    width="16"
+                    height="16"
+                    className={s.iconColumn}
+                  />
+                </div>
+              </button>
+              <button className={`${s.button} ${s.buttonColumnAdd}`} type="button">
+                <div className={`${s.svgWrapperAddCard} ${s.svgWrapper} `}>
+                  <SvgIcon
+                    name="icon-plus"
+                    width="14"
+                    height="14"
+                    className={s.iconAddCard}
+                  />
+                </div>
+                <span className={s.buttonAddCard}>Add another card</span>
+              </button>
+            </div> 
             <button className={s.button} type="button">
               <div className={s.svgWrapper}>
                 <SvgIcon
@@ -27,39 +59,6 @@ const ScreensPage = () => {
               </div>
               <span className={s.buttonSpan}>Add another column</span>
             </button> 
-            <button className={s.button} type="button">
-              <div className={s.svgWrapper}>
-                <SvgIcon
-                  name="icon-plus"
-                  width="14"
-                  height="14"
-                  className={s.icon}
-                />
-              </div>
-              <span className={s.buttonSpan}>Add another column</span>
-            </button>
-            <button className={s.button} type="button">
-              <div className={s.svgWrapper}>
-                <SvgIcon
-                  name="icon-plus"
-                  width="14"
-                  height="14"
-                  className={s.icon}
-                />
-              </div>
-              <span className={s.buttonSpan}>Add another column</span>
-            </button>
-            <button className={s.button} type="button">
-              <div className={s.svgWrapper}>
-                <SvgIcon
-                  name="icon-plus"
-                  width="14"
-                  height="14"
-                  className={s.icon}
-                />
-              </div>
-              <span className={s.buttonSpan}>Add another column</span>
-            </button>
           </div>
         ) : (
           <p className={s.text}>
