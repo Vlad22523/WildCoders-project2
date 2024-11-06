9b807bc3d3e7d10009ad3a3e72703eda694ec5c2
Serhii Syrov <surov.sergey.m@gmail.com>
2024-11-06 13:12:12 +0100

    lazy load

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,71 @@
 import { useSelector } from "react-redux";
-import { selectIsLoggedIn, selectIsRefresh } from "./redux/auth/selectors.js";
-import Loader from "./components/Loader/Loader.jsx";
+import { selectIsLoggedIn } from "./redux/auth/selectors.js";
 import { Navigate, Route, Routes } from "react-router-dom";
-import NotFound from "./pages/NotFound/NotFound.jsx";
-import WelcomePage from "./pages/WelcomePage/WelcomePage.jsx";
-import Auth from "./components/Auth/Auth.jsx";
 import { Toaster } from "react-hot-toast";
-import { PublicRoute } from "./routes/PublicRoute.jsx";
-import { PrivateRoute } from "./routes/PrivateRoute.jsx";
-import HomePage from "./pages/HomePage/HomePage.jsx";
-import ScreensPage from "./components/MainBoard/ScreensPage/ScreensPage.jsx";
+import { lazy, Suspense } from "react";
+import NotFound from "./pages/NotFound/NotFound.jsx";
+import Loader from "./components/Loader/Loader.jsx";
+import PublicRoute from "./routes/PublicRoute.jsx";
+import PrivateRoute from "./routes/PrivateRoute.jsx";
+const WelcomePage = lazy(() => import("./pages/WelcomePage/WelcomePage.jsx"));
+const Auth = lazy(() => import("./components/Auth/Auth.jsx"));
+const HomePage = lazy(() => import("./pages/HomePage/HomePage.jsx"));
+const ScreensPage = lazy(
+  () => import("./components/MainBoard/ScreensPage/ScreensPage.jsx")
+);
 
 function App() {
-  const isRefreshing = useSelector(selectIsRefresh);
   const isLoggedIn = useSelector(selectIsLoggedIn);
 
-  return isRefreshing ? (
-    <Loader />
-  ) : (
+  return (
     <>
-      <Toaster />
-      <Routes>
-        <Route
-          path="/"
-          element={<Navigate to={isLoggedIn ? "/home" : "/welcome"} />}
-        />
+      <Suspense fallback={<Loader />}>
+        <Toaster />
+        <Routes>
+          <Route
+            path="/"
+            element={<Navigate to={isLoggedIn ? "/home" : "/welcome"} />}
+          />
 
-        <Route
-          path="/auth/:id"
-          element={
-            <PublicRoute>
-              <Auth />
-            </PublicRoute>
-          }
-        />
+          <Route
+            path="/auth/:id"
+            element={
+              <PublicRoute>
+                <Auth />
+              </PublicRoute>
+            }
+          />
 
-        <Route
-          path="/welcome"
-          element={
-            <PublicRoute>
-              <WelcomePage />
-            </PublicRoute>
-          }
-        />
+          <Route
+            path="/welcome"
+            element={
+              <PublicRoute>
+                <WelcomePage />
+              </PublicRoute>
+            }
+          />
 
-        <Route
-          path="/home"
-          element={
-            <PrivateRoute>
-              <HomePage />
-            </PrivateRoute>
-          }>
-        <Route
-          path="/home/:boardId"
-          element={
-            <PrivateRoute>
-              <ScreensPage />
-            </PrivateRoute>
-          }
+          <Route
+            path="/home"
+            element={
+              <PrivateRoute>
+                <HomePage />
+              </PrivateRoute>
+            }
+          >
+            <Route
+              path=":boardId"
+              element={
+                <PrivateRoute>
+                  <ScreensPage />
+                </PrivateRoute>
+              }
             />
-            </Route>
+          </Route>
 
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </>
   );
 }
